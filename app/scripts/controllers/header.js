e3e4c24a81025aad03a5e51f6f4b53fd128fc575
Dylan W. Demmers <ddemmers@bskassociates.com>
2017-01-16 10:57:04 -0800

    Refactoring

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,19 +1,19 @@
 (function() {
     'use strict';
-
+/*-------------------------------Header Controller To handle Login/Location routing ----------------------*/
     angular
         .module('gambituiApp')
         .controller('header', header);
 
         function header($location,adalAuthenticationService) {
-            var vm = this;
-            vm.isActive = function (viewLocation) {
-                return viewLocation === $location.path();
+            var vm = this;                                                  //Scope           
+            vm.isActive = function (viewLocation) {                         //Is active is used in index html to view which distribution
+                return viewLocation === $location.path();                   //Is in use. Returns the path of that distributioin
             };
 
             // logout of forgeadminuiApp
             vm.logout = function () {
-                adalAuthenticationService.logOut();
+                adalAuthenticationService.logOut();                         //Handles the logout on button push in index.html
             };
         }
 })();
